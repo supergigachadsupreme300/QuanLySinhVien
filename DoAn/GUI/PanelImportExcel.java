74cfb6372a307e6c0553208a3d1b6e84db644e9f
thanhloccccc <nguyenthanhloc11106@gmail.com>
2026-03-10 02:49:53 +0700

    sửa lại giao diện

--- DoAn/GUI/PanelImportExcel.java
+++ DoAn/GUI/PanelImportExcel.java
@@ -2,47 +2,30 @@ package GUI;
 
 import javax.swing.*;
 import net.miginfocom.swing.MigLayout;
-import java.awt.CardLayout;
+import java.awt.*;
 
 public class PanelImportExcel extends JPanel {
 
-    private JButton btnImportHS;
-    private JButton btnImportDiem;
-
-    private JPanel panelContent;
-    private CardLayout card;
-
     public PanelImportExcel(){
 
-        setLayout(new MigLayout("fill","[grow]","[][grow]"));
+        setLayout(new MigLayout("fill","[grow]","[grow]"));
 
         setBorder(BorderFactory.createTitledBorder("IMPORT EXCEL"));
 
-        // MENU
-        JPanel menu = new JPanel(new MigLayout("insets 0"));
+        JTabbedPane tabs = new JTabbedPane();
 
-        btnImportHS = new JButton("Import Danh sách học sinh");
-        btnImportDiem = new JButton("Import Bảng điểm");
+        tabs.setFont(new Font("Segoe UI", Font.BOLD, 13));
 
-        menu.add(btnImportHS,"gapright 10");
-        menu.add(btnImportDiem);
+        tabs.addTab("Danh sách học sinh", new PanelImportHocSinh());
+        tabs.addTab("Bảng điểm", new PanelImportDiem());
+        tabs.setBackground(Color.WHITE);
+        tabs.setForeground(Color.BLACK);
+        tabs.setBorder(BorderFactory.createEmptyBorder());
 
-        add(menu,"center,wrap");
+        UIManager.put("TabbedPane.selected", new Color(255,255,255));
+        UIManager.put("TabbedPane.contentAreaColor", Color.WHITE);
 
-        // CONTENT
-        card = new CardLayout();
-        panelContent = new JPanel(card);
+        add(tabs,"grow");
 
-        panelContent.add(new PanelImportHocSinh(),"HS");
-        panelContent.add(new PanelImportDiem(),"DIEM");
-
-        add(panelContent,"grow");
-
-        // mặc định
-        card.show(panelContent,"HS");
-
-        // EVENT
-        btnImportHS.addActionListener(e -> card.show(panelContent,"HS"));
-        btnImportDiem.addActionListener(e -> card.show(panelContent,"DIEM"));
     }
 }
