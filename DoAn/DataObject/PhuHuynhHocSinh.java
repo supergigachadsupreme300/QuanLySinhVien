31501bef40c1e00d6b116724c0af8063b175e3a8
Ravsacnocode4years <duongpayday2@gmail.com>
2026-03-09 15:53:49 +0700

    up các file để chạy được quản lí phhs
    hãy check bll và dal, sau đó xem bên formphuhuynh và quản lí học sinh dialog + student_GUI

---
+++ DoAn/DataObject/PhuHuynhHocSinh.java
@@ -0,0 +1,40 @@
+package DataObject;
+
+public class PhuHuynhHocSinh {
+    private String maHS;
+    private String maPH;
+    private String quanHe;
+    private int trangThai; // 1: active, 0: inactive
+
+    public PhuHuynhHocSinh() {
+        maHS = "";
+        maPH = "";
+        quanHe = "";
+        trangThai = 1;
+    }
+
+    public PhuHuynhHocSinh(String maHS, String maPH, String quanHe, int trangThai) {
+        this.maHS = maHS;
+        this.maPH = maPH;
+        this.quanHe = quanHe;
+        this.trangThai = trangThai;
+    }
+
+    // Getters and setters
+    public String getMaHS() { return maHS; }
+    public void setMaHS(String maHS) { this.maHS = maHS; }
+
+    public String getMaPH() { return maPH; }
+    public void setMaPH(String maPH) { this.maPH = maPH; }
+
+    public String getQuanHe() { return quanHe; }
+    public void setQuanHe(String quanHe) { this.quanHe = quanHe; }
+
+    public int getTrangThai() { return trangThai; }
+    public void setTrangThai(int trangThai) { this.trangThai = trangThai; }
+
+    @Override
+    public String toString() {
+        return maHS + " - " + maPH + " (" + quanHe + ")";
+    }
+}
