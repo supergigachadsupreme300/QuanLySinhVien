d0812a1a097dbd58b414de41f36f70dee7d5cdb6
thanhloccccc <nguyenthanhloc11106@gmail.com>
2026-03-10 02:46:32 +0700

    thêm excel all môn

--- DoAn/BusinessLogicLayer/ExcelBLL.java
+++ DoAn/BusinessLogicLayer/ExcelBLL.java
@@ -14,39 +14,41 @@ public class ExcelBLL {
 
     // ===== PREVIEW HỌC SINH =====
     public void previewHocSinh(String path, DefaultTableModel model) {
-
         excelDAL.previewHocSinh(path, model);
-
     }
 
     // ===== IMPORT HỌC SINH =====
     public boolean importHocSinh(File file) {
-
         return excelDAL.importHocSinh(file);
-
     }
 
     // ===== PREVIEW ĐIỂM =====
     public void previewDiem(String path, DefaultTableModel model) {
-
         excelDAL.previewDiem(path, model);
-
     }
 
     // ===== IMPORT ĐIỂM =====
     public boolean importDiem(File file) {
-
         return excelDAL.importDiem(file);
-
     }
 
-    public void previewDiemTheoLopMon(String maLop, String maMon, DefaultTableModel model){
+    // ===== PREVIEW ĐIỂM THEO LỚP + MÔN =====
+    public void previewDiemTheoLopMon(String maLop, String maMon, String maHK, DefaultTableModel model){
         excelDAL.previewDiemTheoLopMon(maLop, maMon, model);
     }
 
+    // ===== EXPORT ĐIỂM THEO LỚP + MÔN =====
     public boolean exportDiemTheoLopMon(String maLop,String maMon, File file) {
-
         return excelDAL.exportDiemTheoLopMon(maLop, maMon, file);
+    }
 
+    // ===== PREVIEW BẢNG ĐIỂM TẤT CẢ MÔN =====
+    public void previewBangDiemTatCaMon(String maLop, String maHK,  DefaultTableModel model){
+        excelDAL.previewBangDiemTatCaMon(maLop, maHK, model);
+    }
+
+    // ===== EXPORT BẢNG ĐIỂM TẤT CẢ MÔN =====
+    public boolean exportBangDiemTatCaMon(String maLop, String maHK, File file){
+        return excelDAL.exportBangDiemTatCaMon(maLop, maHK, file);
     }
 }
